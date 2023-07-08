79075ec986849b88cd7af976cd21d2642fe532b9
namesanjay <sanjaydaadaa@gmail.com>
2023-07-08 19:37:28 +0545

    First

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        bird = New PictureBox()
+        Timer1 = New Timer(components)
+        Scr = New Label()
+        CType(bird, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' bird
+        ' 
+        resources.ApplyResources(bird, "bird")
+        bird.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(0))
+        bird.Name = "bird"
+        bird.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 40
+        ' 
+        ' Scr
+        ' 
+        resources.ApplyResources(Scr, "Scr")
+        Scr.BackColor = Color.BlueViolet
+        Scr.Name = "Scr"
+        ' 
+        ' Form1
+        ' 
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(Scr)
+        Controls.Add(bird)
+        Name = "Form1"
+        CType(bird, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents bird As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Scr As Label
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,161 @@
+﻿Imports System.Formats.Asn1.AsnWriter
+Imports System.Windows.Forms
+Public Class Form1
+    Private gameLoop As Timer
+    Private velocity As Single = -0.9
+    Private gravity As Single = 2.9
+    Private hasCollided As Boolean = False
+    Private pipeVelocity As Double = 5
+    Private Const pipeVerticalGap As Integer = 200
+    Private Const pipeWidth As Integer = 50
+    Private Const pipeHorizontalGap As Integer = 200
+    Private pipes As New List(Of Pipe)()
+    Private score As Integer = 0
+
+    Private Class Pipe
+        Public Property TopPipe As PipeSegment
+        Public Property BottomPipe As PipeSegment
+    End Class
+    Private Class PipeSegment
+        Public Property Top As Single
+        Public Property Left As Single
+        Public Property Width As Single
+        Public Property Height As Single
+    End Class
+
+    Public Sub New()
+        InitializeComponent()
+        gameLoop = New Timer()
+        gameLoop.Interval = 150
+        AddHandler gameLoop.Tick, AddressOf GameLoopCallback
+        gameLoop.Start()
+    End Sub
+
+    Private Sub GameLoopCallback(sender As Object, e As EventArgs)
+        bird.BackColor = Color.Green
+        If hasCollided Then
+            gameLoop.Dispose()
+            bird.BackColor = Color.Purple
+            MessageBox.Show("GAME OVER")
+            ResetGame()
+            Return
+        End If
+        velocity += gravity
+        bird.Top += velocity
+        If bird.Top < 0 Then
+            bird.Top = 0
+            velocity = 0
+        ElseIf bird.Top > Me.Height - bird.Height Then
+            bird.Top = Me.Height - bird.Height
+            velocity = 0
+        End If
+        'For Each pipe In pipes
+        For i As Integer = 0 To pipes.Count - 1
+            Dim Pip As Pipe = pipes(i)
+            Pip.TopPipe.Left -= pipeVelocity
+            Pip.BottomPipe.Left -= pipeVelocity
+            If Pip.TopPipe.Left < 0 Then
+                pipes.RemoveAt(0)
+                pipes.Add(GiveMePipe())
+                If Not hasCollided Then
+                    score += 1
+                    Scr.Text = "Score : " & score
+                End If
+            End If
+            CheckCollision(Pip.TopPipe, bird)
+            CheckCollision(Pip.BottomPipe, bird)
+        Next
+        Me.Invalidate()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Timer1.Enabled = True
+        For i As Integer = 0 To 12
+            pipes.Add(GiveMePipe())
+        Next
+    End Sub
+
+    Private Function GiveMePipe() As Pipe
+        Dim topPipeHeight As Integer = GiveMeRandomHeight(20, Me.Height - pipeVerticalGap)
+        Dim leftOffset As Integer = 100 + 300
+        If pipes.Count > 0 Then
+            Dim lastPipe As Pipe = pipes(pipes.Count - 1)
+            leftOffset = lastPipe.TopPipe.Left + pipeHorizontalGap
+        End If
+        Dim pipe As New Pipe()
+        pipe.TopPipe = New PipeSegment With {
+        .Top = 0,
+        .Left = leftOffset,
+        .Width = pipeWidth,
+        .Height = topPipeHeight
+        }
+        pipe.BottomPipe = New PipeSegment With {
+        .Top = topPipeHeight + pipeVerticalGap,
+        .Left = leftOffset,
+        .Width = pipeWidth,
+        .Height = Me.Height - topPipeHeight - pipeVerticalGap
+         }
+        Return pipe
+    End Function
+
+    Private Sub CheckCollision(box1 As PipeSegment, bird As PictureBox)
+        If box1.Left < bird.Left + bird.Width AndAlso
+        box1.Left + box1.Width > bird.Left AndAlso
+        box1.Top < bird.Top + bird.Height AndAlso
+        box1.Top + box1.Height > bird.Top Then
+            hasCollided = True
+        End If
+    End Sub
+
+    Private Function GiveMeRandomHeight(min As Integer, max As Integer) As Integer
+        Dim rnd As New Random()
+        Return rnd.Next(min, max + 1)
+    End Function
+
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        Dim g As Graphics = e.Graphics
+        For Each pipe In pipes
+            g.FillRectangle(Brushes.Green, pipe.TopPipe.Left,
+            pipe.TopPipe.Top, pipe.TopPipe.Width, pipe.TopPipe.Height)
+            g.FillRectangle(Brushes.Green, pipe.BottomPipe.Left,
+            pipe.BottomPipe.Top, pipe.BottomPipe.Width, pipe.BottomPipe.Height)
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        velocity += gravity
+        bird.Top += velocity
+        'mathi jana nadina
+        If bird.Top < 0 Then
+            bird.Top = 0
+            velocity = 0
+        End If
+        'Tala jana nadina
+        If (bird.Top > Me.Height - 2 * bird.Height) Then
+            bird.Top = Me.Height - 2 * bird.Height
+            velocity = 0
+            'Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Space Then
+            velocity = -20
+        End If
+    End Sub
+    Private Sub ResetGame()
+        bird.Top = Me.Height \ 2
+        hasCollided = False
+        Scr.Text = "Score : 0"
+        velocity = 0
+        pipes.Clear()
+        For i As Integer = 0 To 12
+            pipes.Add(GiveMePipe())
+        Next
+        gameLoop.Start()
+    End Sub
+
+End Class
